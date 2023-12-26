4a8e262bdf51b8476775d051ceadd6808558c728
KaengEE <yopuiqwer@naver.com>
2023-12-26 15:40:37 +0900

    회원가입 페이지

--- src/Pages/AddUser.jsx
+++ src/Pages/AddUser.jsx
@@ -1,5 +1,68 @@
+import { Link } from "react-router-dom";
+
 function AddUser() {
-  return <div>유저추가</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">유저 가입</h2>
+          {/* 입력폼 */}
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              이름
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="form-control mb-3"
+              placeholder="이름 입력"
+              name="name"
+            />
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                유저네임
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                placeholder="유저네임 입력"
+                name="username"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                이메일
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="이메일 입력"
+                name="email"
+              />
+            </div>
+            {/* 버튼 */}
+            <div className="mb-3 text-center">
+              <button
+                type="submit"
+                className="btn btn-outline-primary px-3 mx-2"
+              >
+                가입
+              </button>
+              <Link
+                to="/"
+                type="submit"
+                className="btn btn-outline-danger px-3 mx-2"
+              >
+                취소
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AddUser;
